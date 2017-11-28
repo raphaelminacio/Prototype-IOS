85938806b16a3afce37c637194215cb9c1956e18
Raphael Menezes <31518443+raphaelminacio@users.noreply.github.com>
2017-11-27 21:26:01 -0500

    v1

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -5,6 +5,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var gradeButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
     
     var tableData: [Model] = []
     
@@ -19,12 +21,10 @@ class ViewController: UIViewController {
 //        }
         closeMenu()
     }
-    
+    //================
     @IBAction func menuTapped(_ sender: FloatActionButton) {
         UIView.animate(withDuration: 0.3, animations: {
             
-       
-        
             if self.menuView.transform == .identity {
                 //menu when its open
                 self.closeMenu()
@@ -32,10 +32,23 @@ class ViewController: UIViewController {
                 self.menuView.transform = .identity
             }
         })
+        
+        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations:{
+            if self.menuView.transform == .identity {
+                self.gradeButton.transform = .identity
+                self.addButton.transform = .identity
+            }
+        })
+    }
+    //================
+    @IBAction func gradeResults(_ sender: UIButton) {
     }
     
+    //================
     func closeMenu() {
         menuView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        gradeButton.transform = CGAffineTransform(translationX: 0, y: 15)
+        addButton.transform = CGAffineTransform(translationX: 15, y: 0)
     }
     
 }
